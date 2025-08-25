f7c5305ddddc01939f6be61675cfde28c6cbe18a
IT24102936 <it24102936@my.sliit.lk>
2025-08-25 11:49:03 +0530

    Init: File Structure

---
+++ src/main/java/com/medicalportal/medicalportal/MedicalPortalApplication.java
@@ -0,0 +1,13 @@
+package com.medicalportal.medicalportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalPortalApplication.class, args);
+	}
+
+}
