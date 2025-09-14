dcd7c600cb7491504035ac9640792feed85af181
IT24102936 <it24102936@my.sliit.lk>
2025-09-14 18:27:37 +0530

    Init: Fix Jsp Error

---
+++ src/main/java/com/medicalportal/medicalportal/controller/Main_controller.java
@@ -0,0 +1,16 @@
+package com.medicalportal.medicalportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Main_controller {
+    @GetMapping("/home")
+    public String home() {
+        return "home"; // Maps to index.jsp
+    }
+    @GetMapping("/")
+    public String index() {
+        return "index"; //
+    }
+}
